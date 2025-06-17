5f88818594ba3aedafa14f962b0ffe6d9cb2347f
Xan <xandrabest@gmail.com>
2025-06-17 02:30:11 -0500

    remove addition to console.php

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Modules\Todoist\Jobs\TodoistCommentOnOneTaskJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::job(new TodoistCommentOnOneTaskJob())->everyMinute();
