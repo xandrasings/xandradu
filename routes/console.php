30b8f6824c19af56fb7b3d23601c008103462bc0
Xan <xandrabest@gmail.com>
2025-07-18 01:56:46 -0500

    test run job hourly

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new TodoistCommentOnOneTaskJob())->everyMinute();
+Schedule::job(new TodoistCommentOnOneTaskJob())->hourlyAt(15);
