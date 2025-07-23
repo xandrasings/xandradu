0ca10368cfaa10b219defd33c1b20a35b7e95097
Xan <xandrabest@gmail.com>
2025-07-23 12:52:50 -0500

    tweaks to person register

--- app/Actions/PersonRegisterAction.php
+++ app/Actions/PersonRegisterAction.php
@@ -7,13 +7,27 @@ use Illuminate\Support\Facades\Log;
 
 class PersonRegisterAction
 {
-    public function handle(string $firstName, string $lastName): void
+    public function handle(string $firstName, string $lastName): ?Person
     {
-        $person = Person::firstOrCreate([
+        $people = Person::where([
             'first_name' => $firstName,
             'last_name' => $lastName,
-        ]);
+        ])->get();
 
-        Log::notice("$firstName $lastName is person $person->id");
+        if ($people->count() > 1) {
+            Log::warning("Multiple people named $firstName $lastName exist.");
+            return null;
+        }
+
+        if($people->isEmpty()) {
+            Log::notice("Creating new person $firstName $lastName.");
+            return Person::create([
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+            ]);
+        }
+
+        Log::warning("Person $firstName $lastName already exists.");
+        return $people->first();
     }
 }
