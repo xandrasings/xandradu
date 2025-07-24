4eda7c000c4b2a8315be765732ad79ad0296d8ba
Xan <xandrabest@gmail.com>
2025-07-24 15:27:54 -0500

    adjustments to todoist data architecture

---
+++ database/migrations/2025_07_24_174020_create_todoist_project_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todoist_project_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('todoist_project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('todoist_user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todoist_project_user');
+    }
+};
