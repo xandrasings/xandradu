e51338b8b05e60b08db85a17274c4db527151517
Xan <xandrabest@gmail.com>
2025-07-24 16:04:18 -0500

    adjustments to migrations (tested via full reset)

--- database/migrations/2025_07_24_171326_create_todoist_colors_table.php
+++ database/migrations/2025_07_24_171326_create_todoist_colors_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('todoist_colors', function (Blueprint $table) {
             $table->id();
+            $table->string('code',16);
             $table->string('name',16);
             $table->timestamps();
             $table->softDeletes();
