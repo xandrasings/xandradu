5a2b78cb267a0a34a1f2aa4519600ca79f0322db
Xan <xandrabest@gmail.com>
2025-07-25 12:37:42 -0500

    TodoistUserGetAction.php to get or create matching todoist user model

---
+++ app/Modules/Todoist/Actions/TodoistUserGetAction.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Modules\Todoist\Actions;
+
+use App\Actions\EmailAddressGetAction;
+use App\Models\TodoistUser;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class TodoistUserGetAction
+{
+    protected EmailAddressGetAction $emailAddressGetAction;
+
+    public function __construct()
+    {
+        $this->emailAddressGetAction = new EmailAddressGetAction();
+    }
+
+    public function handle(string $id, string $email, string $name): ?TodoistUser
+    {
+        $emailAddress = $this->emailAddressGetAction->handle($email);
+
+        if (is_null($emailAddress)) {
+            Log::warning("TodoistUserGetAction failed, due to unsuccessful result from EmailAddressGetAction.");
+            return null;
+        }
+
+        $users = TodoistUser::where(['external_id' => $id])->get();
+
+        if (count($users) > 1) {
+            Log::warning("TodoistUserGetAction failed, found too many TodoistUser records matching id $id.");
+            return null;
+        }
+
+        if ($users->isEmpty()) {
+            Log::notice("TodoistUserGetAction creating TodoistUser $id $email $name");
+            try {
+                return TodoistUser::create([
+                    'external_id' => $id,
+                    'email_address_id' => $emailAddress->id,
+                    'name' => $name,
+                ]);
+            } catch (Throwable $exception) {
+                Log::warning("TodoistUserGetAction failed with exception {$exception->getMessage()}");
+                return null;
+            }
+        } else {
+            $user = $users->first();
+
+            if ($user->email->id !== $emailAddress->id) {
+                Log::warning("TodoistUserGetAction found TodoistUser EmailAddress id {$user->email->id} does not match value $emailAddress->id");
+            }
+
+            if ($user->name !== $name) {
+                Log::warning("TodoistUserGetAction found TodoistUser name id $user->name does not match value $name");
+            }
+
+            return $user;
+        }
+    }
+}
