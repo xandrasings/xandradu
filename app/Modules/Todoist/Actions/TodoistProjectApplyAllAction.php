78ae2daab17cc1929254f50a29a53ff4769339ee
Xan <xandrabest@gmail.com>
2025-08-04 18:04:32 -0500

    refactor for stylistic consistency

---
+++ app/Modules/Todoist/Actions/TodoistProjectApplyAllAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Modules\Todoist\Actions;
+
+use App\Models\TodoistAccount;
+use Illuminate\Support\Facades\Log;
+
+class TodoistProjectApplyAllAction
+{
+    protected TodoistProjectApplyAction $projectApplyAction;
+
+    public function __construct()
+    {
+        $this->projectApplyAction = app(TodoistProjectApplyAction::class);
+    }
+
+    public function handle(TodoistAccount $account, array $payloads): bool
+    {
+        $result = collect($payloads)->map(function ($payload) use ($account) {
+            return ! is_null($this->projectApplyAction->handle($account, $payload));
+        })->reduce(function (bool $carry, bool $result) {
+            return $carry && $result;
+        }, true);
+
+        if (! $result) {
+            Log::warning("TodoistProjectApplyAllAction failed.");
+        }
+
+        return $result;
+    }
+}
