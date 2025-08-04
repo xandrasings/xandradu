ccdaa41ee280854d15a73daa99a6d538e80d4b78
Xan <xandrabest@gmail.com>
2025-08-04 09:53:43 -0500

    Titles Optional on Notion Pages and Databases

--- database/migrations/2025_08_04_011519_create_notion_pages_table.php
+++ database/migrations/2025_08_04_011519_create_notion_pages_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('node_id')->constrained(table: 'notion_nodes')->cascadeOnDelete();
             $table->foreignId('location_id')->constrained(table: 'notion_nodes')->cascadeOnDelete();
             $table->string('external_id', 32)->nullable();
-            $table->string('title', 32);
+            $table->string('title', 32)->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
