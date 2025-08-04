79dc29637e17a023876adf7f397d3e2f16cc8ace
Xan <xandrabest@gmail.com>
2025-08-04 17:15:39 -0500

    refactor for stylistic consistency

---
+++ app/Modules/Todoist/Actions/TodoistColorInstantiateAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Todoist\Actions;
+
+use App\Models\TodoistColor;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class TodoistColorInstantiateAction
+{
+    public function handle(string $colorCode, string $name): ?TodoistColor
+    {
+        try {
+            Log::notice("TodoistColorInstantiateAction creating TodoistColor with code $colorCode and name $name.");
+            return TodoistColor::create([
+                'code' => $colorCode,
+                'name' => $name
+            ]);
+        } catch (Throwable $exception) {
+            Log::warning("TodoistColorInstantiateAction failed with exception {$exception->getMessage()}.");
+            return null;
+        }
+    }
+}
