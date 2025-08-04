6bd2a311d0ebd59e87e29009849c9f222394ec4a
Xan <xandrabest@gmail.com>
2025-08-04 17:45:37 -0500

    refactor for stylistic consistency

--- app/Modules/Todoist/Actions/TodoistColorGetAction.php
+++ app/Modules/Todoist/Actions/TodoistColorGetAction.php
@@ -5,40 +5,31 @@ namespace App\Modules\Todoist\Actions;
 use App\Models\TodoistColor;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
-use Throwable;
 
 class TodoistColorGetAction
 {
-    public function handle(string $colorCode): ?TodoistColor
-    {
-        $name = Str::apa(str_replace('_', ' ', $colorCode));
+    protected TodoistColorInstantiateAction $colorInstantiateAction;
 
-        $colors = TodoistColor::where(['code' => $colorCode])->get();
+    public function __construct()
+    {
+        $this->colorInstantiateAction = app(TodoistColorInstantiateAction::class);
+    }
+
+    public function handle(string $code): ?TodoistColor
+    {
+        $name = Str::apa(str_replace('_', ' ', $code));
+
+        $colors = TodoistColor::where(['code' => $code])->get();
 
         if (count($colors) > 1) {
-            Log::warning("TodoistColorGetAction failed, found too many TodoistColor records matching code $colorCode.");
+            Log::warning("TodoistColorGetAction failed, found too many TodoistColor records matching code $code.");
             return null;
         }
 
         if ($colors->isEmpty()) {
-            try {
-                Log::notice("TodoistColorGetAction creating TodoistColor $colorCode $name");
-                return TodoistColor::create([
-                    'code' => $colorCode,
-                    'name' => $name
-                ]);
-            } catch (Throwable $exception) {
-                Log::warning("TodoistColorGetAction failed with exception {$exception->getMessage()}");
-                return null;
-            }
+            return $this->colorInstantiateAction->handle($code, $name);
         }
 
-        $color = $colors->first();
-
-        if ($color->name !== $name) {
-            Log::warning("TodoistColorGetAction found TodoistColor name $color->name does not match value $name");
-        }
-
-        return $color;
+        return $colors->first();
     }
 }
