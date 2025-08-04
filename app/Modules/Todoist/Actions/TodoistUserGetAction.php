1322fe357e0bca8b55148bea3de8346a7884144b
Xan <xandrabest@gmail.com>
2025-08-04 14:03:21 -0500

    general refactor for consistency

--- app/Modules/Todoist/Actions/TodoistUserGetAction.php
+++ app/Modules/Todoist/Actions/TodoistUserGetAction.php
@@ -2,60 +2,34 @@
 
 namespace App\Modules\Todoist\Actions;
 
-use App\Actions\EmailAddressGetAction;
+use App\Models\EmailAddress;
 use App\Models\TodoistUser;
 use Illuminate\Support\Facades\Log;
-use Throwable;
 
 class TodoistUserGetAction
 {
-    protected EmailAddressGetAction $emailAddressGetAction;
-
     protected TodoistUserInstantiateAction $userInstantiateAction;
 
     public function __construct()
     {
-        $this->emailAddressGetAction = app(EmailAddressGetAction::class);
         $this->userInstantiateAction = app(TodoistUserInstantiateAction::class);
     }
 
-    public function handle(string $id, string $email, string $name): ?TodoistUser
+    public function handle(string $id, EmailAddress $emailAddress, string $name): ?TodoistUser
     {
-        $emailAddress = $this->emailAddressGetAction->handle($email);
-
-        if (is_null($emailAddress)) {
-            Log::warning("TodoistUserGetAction failed, due to unsuccessful result from EmailAddressGetAction.");
-            return null;
-        }
-
-        $users = TodoistUser::where(['external_id' => $id])->get();
+        $users = TodoistUser::where([
+            'external_id' => $id
+        ])->get();
 
         if (count($users) > 1) {
-            Log::warning("TodoistUserGetAction failed, found too many TodoistUser records matching id $id.");
+            Log::warning("TodoistUserGetAction failed, found too many TodoistUser records matching external id $id.");
             return null;
         }
 
         if ($users->isEmpty()) {
-            try {
-                Log::notice("TodoistUserGetAction creating TodoistUser $id $email $name");
-
-                return $this->userInstantiateAction->handle($id, $email, $name);
-            } catch (Throwable $exception) {
-                Log::warning("TodoistUserGetAction failed with exception {$exception->getMessage()}");
-                return null;
-            }
-        } else {
-            $user = $users->first();
-
-            if ($user->emailAddress->id !== $emailAddress->id) {
-                Log::warning("TodoistUserGetAction found TodoistUser EmailAddress id {$user->email->id} does not match value $emailAddress->id");
-            }
-
-            if ($user->name !== $name) {
-                Log::warning("TodoistUserGetAction found TodoistUser name id $user->name does not match value $name");
-            }
-
-            return $user;
+            return $this->userInstantiateAction->handle($id, $emailAddress, $name);
         }
+
+        return $users->first();
     }
 }
