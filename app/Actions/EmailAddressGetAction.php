2af4a41bbf960504d2d90ac33b98a5b40109fedf
Xan <xandrabest@gmail.com>
2025-08-04 13:25:37 -0500

    separate EmailAddress instantiate logic

--- app/Actions/EmailAddressGetAction.php
+++ app/Actions/EmailAddressGetAction.php
@@ -4,33 +4,31 @@ namespace App\Actions;
 
 use App\Models\EmailAddress;
 use Illuminate\Support\Facades\Log;
-use Throwable;
 
 class EmailAddressGetAction
 {
-    public function handle(string $email): ?EmailAddress
+    protected EmailAddressInstantiateAction $emailAddressInstantiateAction;
+
+    public function __construct()
     {
-        $emailAddresses = EmailAddress::where(['full_value' => $email])->get();
+        $this->emailAddressInstantiateAction = app(EmailAddressInstantiateAction::class);
+    }
+
+    public function handle(string $fullValue): ?EmailAddress
+    {
+        $emailAddresses = EmailAddress::where([
+            'full_value' => $fullValue
+        ])->get();
 
         if (count($emailAddresses) > 1) {
-            Log::warning("EmailAddressGetAction failed, found too many EmailAddress records matching value $email.");
+            Log::warning("EmailAddressGetAction failed, found too many EmailAddress records matching value $fullValue.");
             return null;
         }
 
         if ($emailAddresses->isEmpty()) {
-            Log::notice("EmailAddressGetAction Creating EmailAddress $email");
-            try {
-                return EmailAddress::create([
-                    'full_value' => $email
-                ]);
-            } catch (Throwable $exception) {
-                Log::warning("EmailAddressGetAction failed with exception {$exception->getMessage()}");
-                return null;
-            }
-        } else {
-            $emailAddress = $emailAddresses->first();
-
-            return $emailAddress;
+            return $this->emailAddressInstantiateAction->handle($fullValue);
         }
+
+        return $emailAddresses->first();
     }
 }
---
+++ app/Actions/EmailAddressInstantiateAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\EmailAddress;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class EmailAddressInstantiateAction
+{
+    public function handle(string $fullValue): ?EmailAddress
+    {
+        try {
+            Log::notice("EmailAddressInstantiateAction creating EmailAddress from $fullValue");
+            return EmailAddress::create([
+                'full_value' => $fullValue,
+            ]);
+        } catch (Throwable $exception) {
+            Log::warning("BandWikiInstantiateAction failed with exception {$exception->getMessage()}");
+            return null;
+        }
+    }
+}
