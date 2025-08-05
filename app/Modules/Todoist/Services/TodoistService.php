8ebafee88af4fd576917d2b4ffa7d9a16dc4aa15
Xan <xandrabest@gmail.com>
2025-08-04 20:47:01 -0500

    refactor for stylistic consistency

--- app/Modules/Todoist/Services/TodoistService.php
+++ app/Modules/Todoist/Services/TodoistService.php
@@ -6,36 +6,36 @@ use App\Models\Person;
 use App\Models\TodoistAccount;
 use App\Models\TodoistSection;
 use App\Modules\Todoist\Actions\TodoistAccountCreateAction;
-use App\Modules\Todoist\Actions\TodoistAccountsSyncAction;
+use App\Modules\Todoist\Actions\TodoistAccountSyncAllAction;
 use App\Modules\Todoist\Actions\TodoistSectionRealizeAction;
 
 class TodoistService
 {
     protected TodoistAccountCreateAction $accountCreateAction;
 
-    protected TodoistAccountsSyncAction $accountsSyncAction;
+    protected TodoistAccountSyncAllAction $accountSyncAllAction;
 
     protected TodoistSectionRealizeAction $sectionRealizeAction;
 
     public function __construct()
     {
         $this->accountCreateAction = app(TodoistAccountCreateAction::class);
-        $this->accountsSyncAction = app(TodoistAccountsSyncAction::class);
+        $this->accountSyncAllAction = app(TodoistAccountSyncAllAction::class);
         $this->sectionRealizeAction = app(TodoistSectionRealizeAction::class);
     }
 
-    public function createAccount(Person $person, string $token): void
+    public function createAccount(Person $person, string $token): ?TodoistAccount
     {
-        $this->accountCreateAction->handle($person, $token);
+        return $this->accountCreateAction->handle($person, $token);
     }
 
-    public function syncAccounts(): void
+    public function syncAccounts(): bool
     {
-        $this->accountsSyncAction->handle();
+        return $this->accountSyncAllAction->handle();
     }
 
-    public function createSection(TodoistAccount $account, TodoistSection $section): void
+    public function createSection(TodoistAccount $account, TodoistSection $section): ?TodoistSection
     {
-        $this->sectionRealizeAction->handle($account, $section);
+        return $this->sectionRealizeAction->handle($account, $section);
     }
 }
