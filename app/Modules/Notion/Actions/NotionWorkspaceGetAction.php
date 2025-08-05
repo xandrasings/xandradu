a23d9b726980656a3ba1a48c3743c14cbb88d39a
Xan <xandrabest@gmail.com>
2025-08-04 21:08:56 -0500

    refactor for stylistic consistency

--- app/Modules/Notion/Actions/NotionWorkspaceGetAction.php
+++ app/Modules/Notion/Actions/NotionWorkspaceGetAction.php
@@ -2,16 +2,27 @@
 
 namespace App\Modules\Notion\Actions;
 
-use App\Models\NotionNode;
 use App\Models\NotionWorkspace;
+use App\Utilities\ValidationUtility;
 use Illuminate\Support\Facades\Log;
-use Throwable;
 
 class NotionWorkspaceGetAction
 {
+    protected ValidationUtility $validationUtility;
+
+    protected NotionWorkspaceInstantiateAction $workspaceInstantiateAction;
+
+    public function __construct()
+    {
+        $this->validationUtility = app(ValidationUtility::class);
+        $this->workspaceInstantiateAction = app(NotionWorkspaceInstantiateAction::class);
+    }
+
     public function handle(string $name): ?NotionWorkspace
     {
-        $workspaces = NotionWorkspace::where(['name' => $name])->get();
+        $workspaces = NotionWorkspace::where([
+            'name' => $name
+        ])->get();
 
         if (count($workspaces) > 1) {
             Log::warning("NotionWorkspaceGetAction failed, found too many NotionWorkspace records matching name $name.");
@@ -19,20 +30,7 @@ class NotionWorkspaceGetAction
         }
 
         if ($workspaces->isEmpty()) {
-            try {
-                Log::notice("NotionWorkspaceGetAction creating NotionWorkspace $name");
-
-                // TODO move as appropriate
-                $node = NotionNode::create([]);
-
-                return NotionWorkspace::create([
-                    'node_id' => $node->id,
-                    'name' => $name
-                ]);
-            } catch (Throwable $exception) {
-                Log::warning("NotionWorkspaceGetAction failed with exception {$exception->getMessage()}");
-                return null;
-            }
+            return $this->workspaceInstantiateAction->handle($name);
         }
 
         return $workspaces->first();
--- app/Modules/Notion/Actions/NotionWorkspaceInstantiateAction.php
+++ app/Modules/Notion/Actions/NotionWorkspaceInstantiateAction.php
@@ -4,13 +4,30 @@ namespace App\Modules\Notion\Actions;
 
 use App\Models\NotionNode;
 use App\Models\NotionWorkspace;
+use App\Utilities\ValidationUtility;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class NotionWorkspaceInstantiateAction
 {
-    public function handle(NotionNode $node, string $name): ?NotionWorkspace
+    protected ValidationUtility $validationUtility;
+
+    protected NotionNodeInstantiateAction $nodeInstantiateAction;
+
+    public function __construct()
     {
+        $this->validationUtility = app(ValidationUtility::class);
+        $this->nodeInstantiateAction = app(NotionNodeInstantiateAction::class);
+    }
+
+    public function handle(string $name): ?NotionWorkspace
+    {
+        $node = $this->nodeInstantiateAction->handle();
+        if (!$this->validationUtility->containsNoNulls([$node])) {
+            Log::warning("NotionWorkspaceInstantiateAction couldn't proceed due to a missing non-nullable variable.");
+            return null;
+        }
+
         try {
             Log::notice("NotionWorkspaceInstantiateAction creating NotionWorkspace from NotionNode $node->id, name $name.");
             return NotionWorkspace::create([
