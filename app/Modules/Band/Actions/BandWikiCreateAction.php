fdfc924f0bc81be5329a84e7c5c2480a64bc7c1e
Xan <xandrabest@gmail.com>
2025-08-04 21:23:00 -0500

    refactor for stylistic consistency

--- app/Modules/Band/Actions/BandWikiCreateAction.php
+++ app/Modules/Band/Actions/BandWikiCreateAction.php
@@ -5,46 +5,33 @@ namespace App\Modules\Band\Actions;
 use App\Models\Band;
 use App\Models\BandWiki;
 use App\Models\NotionWorkspace;
-use App\Modules\Notion\Actions\NotionBotSelectAction;
 use App\Modules\Notion\Actions\NotionPageSyncAction;
-use Illuminate\Support\Facades\Log;
-use Throwable;
 
 class BandWikiCreateAction
 {
-    protected NotionBotSelectAction $botSelectAction;
-
     protected NotionPageSyncAction $notionPageSyncAction;
+
+    protected BandWikiInstantiateAction $wikiInstantiateAction;
 
     public function __construct()
     {
-        $this->botSelectAction = new NotionBotSelectAction();
         $this->notionPageSyncAction = app(NotionPageSyncAction::class);
+        $this->wikiInstantiateAction = app(BandWikiInstantiateAction::class);
     }
 
-    public function handle(Band $band, NotionWorkspace $workspace, string $rootNodeId): ?Band // TODO xan
+    public function handle(Band $band, NotionWorkspace $workspace, string $rootNodeId): ?BandWiki
     {
-        $bot = $this->botSelectAction->handle($workspace,  'xandradu');
 
-        // TODO verify not null
+        // TODO db vs page check, page on fail
 
-        // TODO db check, page on fail
+        $page = $this->notionPageSyncAction->handle($rootNodeId, $workspace);
 
-        $page = $this->notionPageSyncAction->handle($rootNodeId, $bot);
+        $bandWiki = $this->wikiInstantiateAction->handle($band, $page->node);
 
-        $node = $page->node;
+        // TODO null check
 
-        // TODO move this
-        try {
-            BandWiki::create([
-                'band_id' => $band->id,
-                'notion_node_id' => $node->id,
-            ]);
-        } catch (Throwable $exception) {
-            Log::warning("NotionPageInstantiateAction failed with exception {$exception->getMessage()}");
-            return null;
-        }
+        // TODO build up the wiki
 
-        return null;
+        return $bandWiki;
     }
 }
