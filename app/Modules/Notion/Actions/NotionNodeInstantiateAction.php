456d069bd231b0109ebb922373def0fbca662097
Xan <xandrabest@gmail.com>
2025-08-04 21:18:22 -0500

    refactor for stylistic consistency

---
+++ app/Modules/Notion/Actions/NotionNodeInstantiateAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\Notion\Actions;
+
+use App\Models\NotionNode;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class NotionNodeInstantiateAction
+{
+    public function handle(): ?NotionNode
+    {
+
+        try {
+            Log::notice("NotionNodeInstantiateAction creating NotionNode.");
+            return NotionNode::create([]);
+        } catch (Throwable $exception) {
+            Log::warning("NotionNodeInstantiateAction failed with exception {$exception->getMessage()}.");
+            return null;
+        }
+    }
+}
