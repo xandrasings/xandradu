3b9151e1c413a9cedbf7032d32a7060a4c756390
Xan <xandrabest@gmail.com>
2025-08-04 19:55:35 -0500

    refactor for stylistic consistency

--- app/Modules/Todoist/Actions/TodoistProjectUserAssociateAction.php
+++ app/Modules/Todoist/Actions/TodoistProjectUserAssociateAction.php
@@ -9,17 +9,22 @@ use Throwable;
 
 class TodoistProjectUserAssociateAction
 {
-    public function handle(TodoistProject $project, TodoistUser $user, ?string $parentProjectId = null, int $childOrder = 0): bool
+    public function handle(TodoistProject $project, TodoistUser $user, ?TodoistProject $parentProject = null, int $childOrder = 0): bool
     {
+        $parentProjectId = is_null($parentProject) ? null : $parentProject->id;
+
         try {
             // TODO alter this logic to only apply the intermediate table values if we know they're correct
             // TODO alter this logic to still update the intermediate table values even if the relationship is already in place
             if (!$user->projects->contains($project)) {
-                Log::notice("TodoistProjectUserAssociateAction associating TodoistProject $project->id with TodoistUser $user->id.");
-                $user->projects()->attach($project->id, ['parent_project_id' => $parentProjectId, 'rank' => $childOrder]);
+                Log::notice("TodoistProjectUserAssociateAction associating TodoistProject $project->id with TodoistUser $user->id with parent TodoistProject $parentProjectId, child order $childOrder.");
+                $user->projects()->attach($project->id, [
+                    'parent_project_id' => $parentProjectId,
+                    'rank' => $childOrder]
+                );
             }
         } catch (Throwable $exception) {
-            Log::warning("TodoistProjectUserAssociateAction failed with exception {$exception->getMessage()}");
+            Log::warning("TodoistProjectUserAssociateAction failed with exception {$exception->getMessage()}.");
             return false;
         }
         return true;
