10663a2176d0057d4f0185fe03211cfbaa29d9de
Xan <xandrabest@gmail.com>
2025-08-04 21:11:16 -0500

    refactor for stylistic consistency

--- app/Modules/Notion/Actions/NotionPageInstantiateAction.php
+++ app/Modules/Notion/Actions/NotionPageInstantiateAction.php
@@ -2,11 +2,8 @@
 
 namespace App\Modules\Notion\Actions;
 
-use App\Models\NotionNode;
 use App\Models\NotionPage;
 use App\Models\NotionWorkspace;
-use App\Modules\Todoist\Actions\TodoistProjectSelectAction;
-use App\Modules\Todoist\Actions\TodoistTaskLocationCreateAction;
 use App\Utilities\ValidationUtility;
 use Illuminate\Support\Facades\Log;
 use Throwable;
@@ -15,34 +12,31 @@ class NotionPageInstantiateAction
 {
     protected ValidationUtility $validationUtility;
 
-    protected TodoistProjectSelectAction $projectSelectAction;
-
-    protected TodoistTaskLocationCreateAction $taskLocationCreateAction;
+    protected NotionNodeInstantiateAction $nodeInstantiateAction;
 
     public function __construct()
     {
         $this->validationUtility = app(ValidationUtility::class);
-        $this->projectSelectAction = app(TodoistProjectSelectAction::class);
-        $this->taskLocationCreateAction = app(TodoistTaskLocationCreateAction::class);
+        $this->nodeInstantiateAction = app(NotionNodeInstantiateAction::class);
     }
 
     public function handle(array $payload, NotionWorkspace $workspace): ?NotionPage
     {
         $id = data_get($payload, 'id');
         $title = data_get($payload, 'properties.title.title.0.plain_text');
+        if (!$this->validationUtility->containsNoNulls([$id])) {
+            Log::warning("NotionPageInstantiateAction couldn't proceed due to a missing non-nullable variable.");
+            return null;
+        }
 
-        print_r($payload);
-
-        if (! $this->validationUtility->containsNoNulls([$id, $title])) {
-            Log::warning("NotionPageInstantiateAction couldn't proceed due to a missing non-nullable variable");
+        $node = $this->nodeInstantiateAction->handle();
+        if (!$this->validationUtility->containsNoNulls([$node])) {
+            Log::warning("NotionPageInstantiateAction couldn't proceed due to a missing non-nullable variable.");
             return null;
         }
 
         try {
-            // TODO separate out
-            $node = NotionNode::create([]);
-
-            Log::notice("NotionPageInstantiateAction creating NotionPage from $title $id");
+            Log::notice("NotionPageInstantiateAction creating NotionPage from NotionNode $node->id, location $workspace->node->id, external id $id, title $title.");
             return NotionPage::create([
                 'node_id' => $node->id,
                 'location_id' => $workspace->node->id,
@@ -50,7 +44,7 @@ class NotionPageInstantiateAction
                 'title' => $title
             ]);
         } catch (Throwable $exception) {
-            Log::warning("NotionPageInstantiateAction failed with exception {$exception->getMessage()}");
+            Log::warning("NotionPageInstantiateAction failed with exception {$exception->getMessage()}.");
             return null;
         }
     }
