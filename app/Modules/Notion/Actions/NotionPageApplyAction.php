9089e1066e026a20ec1eb03ec4f843da2d74001e
Xan <xandrabest@gmail.com>
2025-08-04 21:17:59 -0500

    refactor for stylistic consistency

--- app/Modules/Notion/Actions/NotionPageApplyAction.php
+++ app/Modules/Notion/Actions/NotionPageApplyAction.php
@@ -4,17 +4,16 @@ namespace App\Modules\Notion\Actions;
 
 use App\Models\NotionPage;
 use App\Models\NotionWorkspace;
-use App\Modules\Notion\Clients\NotionClient;
 use App\Utilities\ValidationUtility;
 use Illuminate\Support\Facades\Log;
 
 class NotionPageApplyAction
 {
-    protected NotionClient $client;
-
     protected ValidationUtility $validationUtility;
 
-    protected NotionBotInstantiateAction $botInstantiateAction;
+    protected NotionPageExistsAction $pageExistsAction;
+
+    protected NotionPageSelectAction $pageSelectAction;
 
     protected NotionPageInstantiateAction $pageInstantiateAction;
 
@@ -22,9 +21,9 @@ class NotionPageApplyAction
 
     public function __construct()
     {
-        $this->client = app(NotionClient::class);
         $this->validationUtility = app(ValidationUtility::class);
-        $this->botInstantiateAction = app(NotionBotInstantiateAction::class);
+        $this->pageExistsAction = app(NotionPageExistsAction::class);
+        $this->pageSelectAction = app(NotionPageSelectAction::class);
         $this->pageInstantiateAction = app(NotionPageInstantiateAction::class);
         $this->pageUpdateAction = app(NotionPageUpdateAction::class);
     }
@@ -32,29 +31,22 @@ class NotionPageApplyAction
     public function handle(array $payload, NotionWorkspace $workspace): ?NotionPage
     {
         $id = data_get($payload, 'id');
-
         if (! $this->validationUtility->containsNoNulls([$id])) {
-            Log::warning("NotionPageApplyAction couldn't proceed due to a missing non-nullable variable");
+            Log::warning("NotionPageApplyAction couldn't proceed due to a missing non-nullable variable.");
             return null;
         }
-
         $id = str_replace('-', '', $id);
 
-        $pages = NotionPage::where(['external_id' => $id])->get();
+        if ($this->pageExistsAction->handle($id)) {
+            $page = $this->pageSelectAction->handle($id);
+            if (!$this->validationUtility->containsNoNulls([$page])) {
+                Log::warning("NotionPageApplyAction couldn't proceed due to a missing non-nullable variable.");
+                return null;
+            }
 
-        // TODO consider in_trash and archived for potential soft delete trigger
-
-        if (! $this->validationUtility->containsNoMoreThanOne($pages)) {
-            Log::warning("NotionPageApplyAction couldn't proceed due to multiple NotionPages matching this external id.");
-            return null;
+            return $this->pageUpdateAction->handle($page, $payload);
         }
 
-        if ($pages->isEmpty()) {
-            return $this->pageInstantiateAction->handle($payload, $workspace);
-        }
-
-        $page = $pages->first();
-
-        return $this->pageUpdateAction->handle($page, $payload);
+        return $this->pageInstantiateAction->handle($payload, $workspace);
     }
 }
