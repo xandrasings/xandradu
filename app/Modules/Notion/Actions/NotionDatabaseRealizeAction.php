63ed26299d4933099bd10360078c13d7e6b3f303
Xan <xandrabest@gmail.com>
2025-10-04 19:54:58 -0500

    change null check logic

--- app/Modules/Notion/Actions/NotionDatabaseRealizeAction.php
+++ app/Modules/Notion/Actions/NotionDatabaseRealizeAction.php
@@ -6,8 +6,7 @@ use App\Modules\Notion\Clients\NotionClient;
 use App\Modules\Notion\Models\NotionBot;
 use App\Modules\Notion\Models\NotionDatabase;
 use App\Utilities\ValidationUtility;
-use Illuminate\Support\Facades\Log;
-use Throwable;
+use Exception;
 
 class NotionDatabaseRealizeAction
 {
@@ -21,29 +20,18 @@ class NotionDatabaseRealizeAction
         $this->validationUtility = app(ValidationUtility::class);
     }
 
-    public function handle(NotionDatabase $database, NotionBot $bot): bool
+    /**
+     * @throws Exception
+     */
+    public function handle(NotionDatabase $database, NotionBot $bot): void
     {
         $response = $this->client->createDatabase($database, $bot);
-        if (is_null($response)) {
-            Log::warning("NotionDatabaseRealizeAction failed due to unsuccessful client call.");
-            return false;
-        }
 
         $id = data_get($response, 'id');
-        if (! $this->validationUtility->containsNoNulls([$id])) {
-            Log::critical("NotionDatabaseRealizeAction failed due to a missing non-nullable variable");
-            return false;
-        }
+        $this->validationUtility->isNotNull($id);
 
-        try {
-            $database->update([
-                'external_id' => $id,
-            ]);
-        } catch (Throwable $exception) {
-            Log::critical("NotionDatabaseRealizeAction failed with exception {$exception->getMessage()}.");
-            return false;
-        }
-
-        return true;
+        $database->update([
+            'external_id' => $id,
+        ]);
     }
 }
