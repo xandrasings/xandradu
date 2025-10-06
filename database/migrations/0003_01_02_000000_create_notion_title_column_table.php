c72e8463fb242bee70619f68610808d8e073e7b3
Xan <xandrabest@gmail.com>
2025-10-06 15:53:18 -0500

    notion column storage for title

---
+++ database/migrations/0003_01_02_000000_create_notion_title_column_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notion_title_column', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->foreignId('column_id')->constrained(table: 'notion_columns')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notion_title_column');
+    }
+};
