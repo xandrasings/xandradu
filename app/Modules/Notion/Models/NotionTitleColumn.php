b7933ccf36fe3d892d4ed08cdd00f149d8cdd695
Xan <xandrabest@gmail.com>
2025-10-13 15:20:37 -0500

    NotionTitleColumn model basics

--- app/Modules/Notion/Models/NotionTitleColumn.php
+++ app/Modules/Notion/Models/NotionTitleColumn.php
@@ -3,10 +3,24 @@
 namespace App\Modules\Notion\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use stdClass;
 
 class NotionTitleColumn extends Model
 {
     protected $fillable = [
         'column_id',
     ];
+
+    public function column(): BelongsTo
+    {
+        return $this->belongsTo(NotionColumn::class, 'column_id');
+    }
+
+    public function getBody(): array
+    {
+        return [
+            'title' => new stdClass()
+        ];
+    }
 }
