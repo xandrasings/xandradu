81548cbae3e2abf6c54d00489a5fa1925b838f53
xandrasings <xandrabest@gmail.com>
2026-02-19 18:16:42 -0600

    composer updates

---
+++ database/migrations/0002_01_01_000000_create_airtable_bases_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('airtable_bases', function (Blueprint $table) {
+            $table->id();
+            $table->string('external_id', 32)->nullable();
+            $table->string('name', 32);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('airtable_bases');
+    }
+};
