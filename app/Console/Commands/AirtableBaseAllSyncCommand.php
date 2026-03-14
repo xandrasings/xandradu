9bc2e360c756f24a66e8d7ee0a1d14c36a982513
xandrasings <xandrabest@gmail.com>
2026-03-13 23:18:07 -0500

    add a command to run airtable sync

---
+++ app/Console/Commands/AirtableBaseAllSyncCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Modules\Airtable\Jobs\AirtableBaseAllSyncJob;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class AirtableBaseAllSyncCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:airtable-base-all-sync-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run job to sync Airtable';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        dispatch(new AirtableBaseAllSyncJob());
+
+        print_r("CONSOLE COMMAND COMPLETED: $this->signature\n");
+        Log::notice("CONSOLE COMMAND COMPLETED: $this->signature");
+    }
+}
