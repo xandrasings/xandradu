87b0caeaf4132942768c6fcfb98f6277f3f6bc01
xandrasings <xandrabest@gmail.com>
2026-03-17 20:44:32 -0500

    add db components for ai text fields

---
+++ database/migrations/1_000_1_02_1_00_10_create_airtable_ai_text_field_prompt_components_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('airtable_ai_text_field_prompt_components', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ai_text_field_id')->nullable()->constrained(table: 'airtable_ai_text_fields', indexName: 'airtable_ai_text_field_components_ai_text_field_id_foreign')->cascadeOnDelete();
+            $table->string('type', 8);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('airtable_ai_text_field_prompt_components');
+    }
+};
---
+++ database/migrations/1_000_1_02_1_00_11_create_airtable_ai_text_field_field_prompt_components_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('airtable_ai_text_field_field_prompt_components', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('prompt_component_id')->nullable()->constrained(table: 'airtable_ai_text_field_prompt_components', indexName: 'airtable_ai_text_field_field_components_ai_text_field_id_foreign')->cascadeOnDelete();
+            $table->foreignId('field_id')->nullable()->constrained(table: 'airtable_fields')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('airtable_ai_text_field_field_prompt_components');
+    }
+};
