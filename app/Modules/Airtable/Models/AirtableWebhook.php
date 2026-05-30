43b06a35493210d4dda9f50fa48b0db4ce52e180
xandrasings <xandrabest@gmail.com>
2026-05-30 15:35:00 -0500

    fix attribute name

--- app/Modules/Airtable/Models/AirtableWebhook.php
+++ app/Modules/Airtable/Models/AirtableWebhook.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\Airtable\Models;
 
-use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -14,12 +13,13 @@ use Illuminate\Support\Carbon;
  * @property int $id
  * @property int $base_id
  * @property string|null $external_id
- * @property string|null $mac_secret
+ * @property string|null $encoded_mac_secret
  * @property Carbon|null $expires_at
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property Carbon|null $deleted_at
  * @property-read AirtableBase $base
+ *
  * @method static Builder<static>|AirtableWebhook newModelQuery()
  * @method static Builder<static>|AirtableWebhook newQuery()
  * @method static Builder<static>|AirtableWebhook onlyTrashed()
@@ -27,13 +27,14 @@ use Illuminate\Support\Carbon;
  * @method static Builder<static>|AirtableWebhook whereBaseId($value)
  * @method static Builder<static>|AirtableWebhook whereCreatedAt($value)
  * @method static Builder<static>|AirtableWebhook whereDeletedAt($value)
+ * @method static Builder<static>|AirtableWebhook whereEncodedMacSecret($value)
  * @method static Builder<static>|AirtableWebhook whereExpiresAt($value)
  * @method static Builder<static>|AirtableWebhook whereExternalId($value)
  * @method static Builder<static>|AirtableWebhook whereId($value)
- * @method static Builder<static>|AirtableWebhook whereMacSecret($value)
  * @method static Builder<static>|AirtableWebhook whereUpdatedAt($value)
  * @method static Builder<static>|AirtableWebhook withTrashed(bool $withTrashed = true)
  * @method static Builder<static>|AirtableWebhook withoutTrashed()
+ *
  * @mixin Eloquent
  */
 class AirtableWebhook extends Model
